9eb316ef3cb7197cfd72a2c3445e00545212f885
Dmytro Horetskyi <horetskiy.d.s.7@gmail.com>
2026-02-28 03:20:30 +0200

    GA added

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
 import { notFound } from "next/navigation";
@@ -41,6 +42,20 @@ export default async function LocaleLayout({ children, params }: Props) {
 
   return (
     <html lang={locale}>
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-BV1E59VBZ6"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-BV1E59VBZ6');
+          `}
+        </Script>
+      </head>
       <body className="antialiased min-h-screen flex flex-col font-sans">
         <NextIntlClientProvider messages={messages}>
           <Header />
